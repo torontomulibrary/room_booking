baf7410708c908bc931ff2854125675c3c3347a5
Steven Marsden <steven.marsden@ryerson.ca>
2015-02-02 14:15:54 -0500

    Cache hours from coldfusion

--- application/models/hours_model.php
+++ application/models/hours_model.php
@@ -7,23 +7,49 @@ class hours_Model  extends CI_Model  {
 	}
 	
 	function getAllHours($date){
-		//Prepare the date into Coldfusion's horrible timestamp
-		$timestamp = "{ts '". date('Y-m-d', $date) . " 00:00:00'}";
+		//Check to see if a cache file already exists
+		if(file_exists('temp/'. date('Ymd', $date).'.hours')){
+			$jsonData = @file_get_contents('temp/'. date('Ymd', $date).'.hours');
+		}
+		else{
+			
+			//Prepare the date into Coldfusion's horrible timestamp
+			$timestamp = "{ts '". date('Y-m-d', $date) . " 00:00:00'}";
+			
+			$opts = array(
+			  'http'=>array(
+				'method'=>"GET",
+				'header'=>"User-Agent: Library Room Booking\r\n" 
+			  ),
+			);
+
+			$context = stream_context_create($opts);
+
+			$url = HOURS_URL . '?dt='.urlencode($timestamp).'&l=all';
+			
+			
+			
+			$jsonData = @file_get_contents($url, false, $context);
+			
+			if($jsonData === FALSE){ 
+				return FALSE; 
+			}
+			
+			//Write it to a file
+			file_put_contents('temp/'. date('Ymd', $date).'.hours', $jsonData);
+		}
 		
-		$opts = array(
-		  'http'=>array(
-			'method'=>"GET",
-			'header'=>"User-Agent: Library Room Booking\r\n" 
-		  ),
-		);
-
-		$context = stream_context_create($opts);
-
-		//We need to find the MIN and MAX time for the current users roles
+		$hours_json = json_decode($jsonData);
+		
+		//Make sure it is valid JSON
+		if($hours_json === null) return FALSE;
+		
+		$output = array();
+		
 		$min = 2; //In coldfusion, 1 is midnight of the next day. Use 2 (instead of 1 in this case to cover hours such as 1am)
 		$max = -1; //In coldfusion, 0 is midnight
 		
-		//First, find the external ID'd for all buildings the user can book
+		//Load all of the external ID's
 		$this->load->model('room_model');
 		$rooms = $this->room_model->list_rooms(true);
 		
@@ -33,43 +59,29 @@ class hours_Model  extends CI_Model  {
 				$buildings[] = $room->external_id;
 			}
 		}
-		
-		$url = HOURS_URL . '?dt='.urlencode($timestamp).'&l=all';
-		
-		
-		
-		$jsonData = @file_get_contents($url, false, $context);
-		
-		if($jsonData !== FALSE){
-			//var_dump(json_decode($jsonData)); die;
+
+		//Match the external ID's with those in the JSON result
+		foreach($hours_json as $location){
+			$output[$location->LOCATION_ID] = $location->DATA;
 			
-			$hours_json = json_decode($jsonData);
-			
-			$output = array();
-			
-			foreach($hours_json as $location){
-				$output[$location->LOCATION_ID] = $location->DATA;
-				
-				if(in_array($location->LOCATION_ID, $buildings)){
-					if($location->DATA->STARTTIME == $location->DATA->ENDTIME || $location->DATA->ISOPEN == false || $location->DATA->HASCLOSURE == true){
-						//Assume the building is closed in this situation
-						continue;
-					}
-					
-					//Should we factor in the "ISOPEN" property here?
-					if($location->DATA->STARTTIME < $min) $min = $location->DATA->STARTTIME;
-					if($location->DATA->ENDTIME > $max) $max = $location->DATA->ENDTIME;
+			if(in_array($location->LOCATION_ID, $buildings)){
+				//Is the building closed?
+				if($location->DATA->STARTTIME == $location->DATA->ENDTIME || $location->DATA->ISOPEN == false || $location->DATA->HASCLOSURE == true){
+					//Delete the cache file, as the user may be looking too far into the future where the hours have not yet been entered
+					@unlink('temp/'. date('Ymd', $date).'.hours');					
+					continue;
 				}
+				
+				//Should we factor in the "ISOPEN" property here?
+				if($location->DATA->STARTTIME < $min) $min = $location->DATA->STARTTIME;
+				if($location->DATA->ENDTIME > $max) $max = $location->DATA->ENDTIME;
 			}
-			
-			$output['min'] = $min;
-			$output['max'] = $max;
-			
-		//	var_dump($output);
-			
-			return $output;
 		}
 		
-		return FALSE;		
+		$output['min'] = $min;
+		$output['max'] = $max;
+		
+		return $output;
+
 	}
 }
