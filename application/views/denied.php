6e6aa47d9eecebf398dfb1dd9089765622eae23f
Steven Marsden <steven.marsden@ryerson.ca>
2015-06-19 10:17:22 -0400

    Added link to policy

--- application/views/denied.php
+++ application/views/denied.php
@@ -8,6 +8,8 @@
 <span style="text-align: center">
 	<p>You do not have access to the Library Room Booking System</p>
 
+	<p>Please read the <a href="http://library.ryerson.ca/info/policies/study-room-booking-policy/">policy</a> to see if you should have access</p>
+	
 	<p>If you believe you have reached this page in error, please contact <a href="mailto:<?php echo SITE_ADMIN; ?>"><?php echo SITE_ADMIN; ?></a></p>
 
 	<p><a href="<?php echo base_url(); ?>logout">Logout</a></p>
