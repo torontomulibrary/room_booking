61b7030b626f7bef375ce040cf0f461305589685
Steven Marsden <steven.marsden@ryerson.ca>
2015-10-09 11:11:22 -0400

    fixed untacked files

--- application/config/cas.php
+++
@@ -1,4 +0,0 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$config['cas_server_url'] = 'https://cas.spi.ryerson.ca';
-$config['phpcas_path'] = 'C:\wamp\www\room_booking\phpCas';
-$config['cas_disable_server_validation'] = TRUE;
