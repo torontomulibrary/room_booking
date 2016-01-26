e8adf57dfb1d05b24028dabb9bfaca410279b972
Steven Marsden <steven.marsden@ryerson.ca>
2016-01-26 10:18:37 -0500

    Fixed bug where limits check the wrong week
    Eliminated the need to compare PHP & MYSQL results for determining what
    week of the year it is

--- application/models/booking_model.php
+++ application/models/booking_model.php
@@ -80,13 +80,10 @@ class booking_Model  extends CI_Model  {
 		
 		$limits = $this->db->query($sql)->row();
 		
-		$week = date('W', $date) - 1; //Subtract 1, since mysql treats "0" as the first week
-		
-		
 		
 		//Pull down their existing bookings for that week (don't cache this)
 		$this->db->cache_off();
-		$weekly_bookings_query = $this->db->query("SELECT IFNULL(sum(TIMESTAMPDIFF(minute,start,end)),0) as weekly_minutes FROM bookings where matrix_id = ". $this->db->escape($this->session->userdata('username')). " AND  week(start,0) = " . $week);
+		$weekly_bookings_query = $this->db->query("SELECT IFNULL(sum(TIMESTAMPDIFF(minute,start,end)),0) as weekly_minutes FROM bookings where matrix_id = ". $this->db->escape($this->session->userdata('username')). " AND  yearweek(start,6) = yearweek('" . date('Y-m-d',$date)."',6)";
 		$this->db->cache_on();
 		$weekly_bookings = $weekly_bookings_query->row();
 		
