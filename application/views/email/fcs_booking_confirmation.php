b2f5c9eb86efc7db7c365ed7040b49760f6331a4
Steven Marsden <steven.marsden@ryerson.ca>
2019-11-12 13:01:41 -0500

    Template update

--- application/views/email/fcs_booking_confirmation.php
+++ application/views/email/fcs_booking_confirmation.php
@@ -10,9 +10,9 @@ To view/cancel your booking, please click the following <a href="<?php echo base
 
 Please note, you must claim your room within the first 15 minutes of the booking, or your room may be taken by other eligible users. <br><br>
 
-In order to access the room, please pick up the room key from the Faculty of Community Services Dean's Office, located in SHE-697.<br><br>
+In order to access the room, please pick up keys from the DCC Operations Office located on the 8th floor, DCC815.<br><br>
 
-Please have your one card ready, and check in at the front desk with the Administrative Assistant. Please note that you will need to leave your one card to sign out the room key.<br><br>
+Please have your one card ready and be prepared to sign the key loan agreement form with one of the DCC Specialists.<br><br>
 
 
 <b>Be sure to lock the door when you exit the room.</b>
